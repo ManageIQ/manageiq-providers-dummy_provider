463ff40bb4cad8ffb55384d8767e5af887463477
Adam Grare <agrare@redhat.com>
2018-07-30 15:22:06 -0400

    Add a simple ansible-runner playbook

--- app/models/manageiq/providers/dummy_provider/cloud_manager/vm.rb
+++ app/models/manageiq/providers/dummy_provider/cloud_manager/vm.rb
@@ -14,6 +14,29 @@ class ManageIQ::Providers::DummyProvider::CloudManager::Vm < ManageIQ::Providers
     update_attributes!(:raw_power_state => "on")
   end
 
+  def dummy
+    raw_dummy
+  end
+
+  def raw_dummy
+    env_vars = {
+      "PROVIDER_USERID"   => "root",
+      "PROVIDER_PASSWORD" => "password",
+    }
+    extra_vars = {
+      :id   => id,
+      :name => name,
+    }
+    playbook_path = ext_management_system.ansible_root.join("dummy.yml")
+
+    result = Ansible::Runner.run(env_vars, extra_vars, playbook_path)
+    if result.return_code != 0
+      _log.error("Failed to start VM: #{result.parsed_stdout}")
+    else
+      _log.info("Dummied VM [#{id}] [#{name}]")
+    end
+  end
+
   def raw_stop
     with_provider_object(&:stop)
     # Temporarily update state for quick UI response until refresh comes along
