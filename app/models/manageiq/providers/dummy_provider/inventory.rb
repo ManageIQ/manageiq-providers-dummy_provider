a5cdc0eec052bc7f5334637a2f27ffb8444f39ba
Adam Grare <agrare@redhat.com>
2018-08-27 13:34:57 -0400

    Move Inventory to ManageIQ::Providers
    Move ManagerRefresh::Inventory to ManageIQ::Providers::Inventory

--- app/models/manageiq/providers/dummy_provider/inventory.rb
+++ app/models/manageiq/providers/dummy_provider/inventory.rb
@@ -1,4 +1,4 @@
-class ManageIQ::Providers::DummyProvider::Inventory < ManagerRefresh::Inventory
+class ManageIQ::Providers::DummyProvider::Inventory < ManageIQ::Providers::Inventory
   require_nested :Collector
   require_nested :Parser
   require_nested :Persister
