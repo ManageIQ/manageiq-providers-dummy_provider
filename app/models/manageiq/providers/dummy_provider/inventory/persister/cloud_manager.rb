d9190f48c47fc192da05a6aca3d4237813565af3
Adam Grare <agrare@redhat.com>
2019-01-04 15:38:12 -0500

    Remove duplicated persister shared_options
    The shared_options were duplicated from the base persister

--- app/models/manageiq/providers/dummy_provider/inventory/persister/cloud_manager.rb
+++ app/models/manageiq/providers/dummy_provider/inventory/persister/cloud_manager.rb
@@ -3,14 +3,4 @@ class ManageIQ::Providers::DummyProvider::Inventory::Persister::CloudManager < M
     add_collection(cloud, :vms)
     add_collection(cloud, :flavors)
   end
-
-  def parent
-    manager.presence
-  end
-
-  def shared_options
-    {
-      :parent => parent
-    }
-  end
 end
